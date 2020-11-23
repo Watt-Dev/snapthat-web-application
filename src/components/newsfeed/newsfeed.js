fbd1645887360b5ffa1c263acb9bdb082d815d93
William Thorenfeldt <wrt_95@hotmail.com>
2020-11-23 12:27:29 +0000

    fixing newsfeed design

--- src/components/newsfeed/newsfeed.js
+++ src/components/newsfeed/newsfeed.js
@@ -1,35 +1,73 @@
 import React from "react";
 import './newsfeed.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAngleDown } from '@fortawesome/free-solid-svg-icons'
-import { faStream } from '@fortawesome/free-solid-svg-icons'
-import { faSearch } from '@fortawesome/free-solid-svg-icons'
-import { faCameraRetro } from '@fortawesome/free-solid-svg-icons'
-import { faCog } from '@fortawesome/free-solid-svg-icons'
+import {
+    faAngleDown,
+    faStream,
+    faSearch,
+    faCameraRetro,
+    faCog, faArrowCircleUp, faArrowCircleDown, faComments,
+} from '@fortawesome/free-solid-svg-icons'
+
 
 import snapThatWhite from '../../images/snapthatWhite.png';
-import appLogo from '../../images/AppLogo.png'
+import dummyImage from "../../images/dummyPostImage.png";
 
 const NewsFeed = () => {
     const snapthatWhiteLogo = snapThatWhite;
 
+    // TODO: Change so it takes the correct images
+    const numberOfImagesToDisplay = 8;
+    const showImage = [...Array(numberOfImagesToDisplay)].map((image, index) =>
+        <div className="newsfeed-posts" key={index}>
+            <img src={dummyImage} className="newsfeed-image" alt="logo"/>
+            <div className="newsfeed-post-text">
+                <div className="post-title">
+                    Post Title
+                </div>
+                <div className="post-user">
+                    @postUser
+                </div>
+                <div className="post-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                    enim ad minim veniam, quis nostrud exercitation
+                </div>
+                <div className="post-interact">
+                    <div className="post-interact-vote">
+                        238
+                        <FontAwesomeIcon className="arrow-circle-up-icon" icon={faArrowCircleUp} />
+                        <FontAwesomeIcon className="arrow-circle-down-icon" icon={faArrowCircleDown} />
+                    </div>
+                    <div className="post-interact-comment">
+                        34
+                        <FontAwesomeIcon className="comments-icon" icon={faComments} />
+                    </div>
+                </div>
+            </div>
+        </div>);
+
     return (
         <div className="newsfeed">
             <div className="top-area">
-                <span className="logo">
-                    <img src={snapthatWhiteLogo} className="logo-white" alt="logo" />
-                </span>
-                <span className="title">
-                    <h2 className="title-name">
-                        SNAPTHAT
-                    </h2>
-                </span>
                 <span className="user-menu">
                      <FontAwesomeIcon className="angle-down-icon" icon={faAngleDown} />
                      <div className="user-menu-text">
                          Hi, user
                      </div>
                 </span>
+                <div>
+                    <span className="logo">
+                        <img src={snapthatWhiteLogo} className="logo-white" alt="logo" />
+                    </span>
+                        <span className="title">
+                        <h2 className="title-name">
+                            SNAPTHAT
+                        </h2>
+                    </span>
+                </div>
+
+
             </div>
         <div className="menu-area">
             <div className="menu-area-item">
@@ -60,25 +98,7 @@ const NewsFeed = () => {
         <div className="newsfeed-body">
             {/* TODO: Create function tto get images, then replace the duplicates with */}
             {/* TODO: only one newsfeed post div, which displays several images*/}
-
-            <div className="newsfeed-posts">
-                <img src={appLogo} className="newsfeed-image" alt="logo" />
-                <div className="newsfeed-post-text">
-                    TODO: Add stuff for the post
-                </div>
-            </div>
-            <div className="newsfeed-posts">
-                <img src={appLogo} className="newsfeed-image" alt="logo" />
-                <div className="newsfeed-post-text">
-                    TODO: Add stuff for the post
-                </div>
-            </div>
-            <div className="newsfeed-posts">
-                <img src={appLogo} className="newsfeed-image" alt="logo" />
-                <div className="newsfeed-post-text">
-                    TODO: Add stuff for the post
-                </div>
-            </div>
+            {showImage}
         </div>
     </div>
     )
