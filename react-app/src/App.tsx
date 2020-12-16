4dd9562f0b6e9ba40a07ccc239659de1dc115a0f
William Thorenfeldt <wrt_95@hotmail.com>
2020-12-16 12:57:55 +0100

    adding button switch

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,24 +1,36 @@
-import React from "react";
+import React, {useState} from "react";
 import './App.css';
 import NewsFeed from "./components/newsfeed/newsfeed";
 import TopBar from "./components/TopBars";
 import CreatePost from "./components/createpost/createPost";
+import TopBar2 from "./components/TopBars2";
 
 // eslint-disable-next-line
 const App = () => {
 
-    const currentTab = "newsfeed";
+
+    const [tab, setTab] = useState("newsfeed")
+    const tabNames = ["newsfeed", "create", "search"]
+
+    const onClick = ( () => {
+        if (tab === "newsfeed")
+            setTab("create");
+        else
+            setTab("newsfeed");
+        }
+    )
 
     return (
         <div className="App">
-            <TopBar currentTab={currentTab}/>
-                {(currentTab === "newsfeed") ?
+            <TopBar2 onClick={onClick} text={tab} tabNames={tabNames}/>
+            {/*<TopBar currentTab={currentTab}/>*/}
+                {(tab === "newsfeed") ?
                     <NewsFeed />
-                : (currentTab === "search") ?
+                : (tab === "search") ?
                     <p>Coming soon</p>
-                :(currentTab === "create") ?
+                :(tab === "create") ?
                     <CreatePost />
-                : (currentTab === "settings") ?
+                : (tab === "settings") ?
                      <p>Coming soon</p>
                 : <p>TODO</p>
             }
